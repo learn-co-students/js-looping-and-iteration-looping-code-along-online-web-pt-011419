0ec79eef639ec8d1df574aaa894da27db03796e0
imaglazer <avigayilglazer@gmail.com>
2019-07-29 21:14:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// const stringNames = ['Lisa', 'Kaitlin', 'Jan']
+// const eventName = 'surprise'
+
+function writeCards(stringNames, eventName) {
+    let thankYouArray = []
+    for (let i = 0; i < stringNames.length; i++) {
+        thankYouArray.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+
+    return thankYouArray
+    
+}
+
+function countdown(integer) {
+    while (integer >= 0) {
+        console.log(integer--)
+    }
+}
