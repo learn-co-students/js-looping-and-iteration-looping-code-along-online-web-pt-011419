907b3c1b7ff976cf237dd27c5d497f8ca511002c
dclements9 <dclements@protonmail.com>
2019-07-31 10:40:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let thankyouArray = []
+
+function writeCards(stringsArray, eventName) {
+    for (let i = 0; i < stringsArray.length; i++){
+       thankyouArray.push(`Thank you, ${stringsArray[i]}, for the wonderful surprise gift!`)
+    }
+    return thankyouArray;
+}
+
+function countdown(int){
+    while (int > 0){
+        console.log(int);
+        int -= 1;
+    }
+    console.log(int);
+}
